2684345a585107b3ad74a4fb55f4e1832ee3e716
jchen1379 <jianhuichen1379@gmail.com>
2021-11-24 00:01:39 -0500

    writing output to csv completed

--- run_analysis.py
+++ run_analysis.py
@@ -62,12 +62,15 @@ def run():
 
         model.set_FCC_growth_rate(short_term_growth_rate, medium_term_growth_rate, long_term_growth_rate)
 
+        eps = row['EPS Next 5Y in percent']
         fair_value = model.calc_fair_value()
         current_price = stock.yfinancial.get_current_price()
-        sector = yf.Ticker(stock.symbol).info
+        sector = yf.Ticker(stock.symbol).info['sector']
         market_cap = stock.yfinancial.get_market_cap()
         beta = stock.get_beta()
-        total_assets = list(stock.yfinancial.get_financial_stmts('annual', 'balance').get('balanceSheetHistory')[stock.symbol][0].values())[0]['totalAssets']
+        total_assets = list(
+            stock.yfinancial.get_financial_stmts('annual', 'balance').get('balanceSheetHistory')[stock.symbol][
+                0].values())[0]['totalAssets']
         total_debt = stock.get_total_debt()
         free_cash_flow = stock.get_free_cashflow()
         p_e_ratio = stock.yfinancial.get_pe_ratio()
@@ -84,8 +87,16 @@ def run():
         smas50 = smas.get_series(50)
         smas200 = smas.get_series(200)
 
-    # save the output into a StockUniverseOutput.csv file
+        results.append(
+            [stock.symbol, eps, fair_value, current_price, sector, market_cap, beta, total_assets, total_debt,
+             free_cash_flow, p_e_ratio, price_to_sale_ratio, rsi, ema10, smas20, smas50, smas200])
 
+    # save the output into a StockUniverseOutput.csv file
+    header = ['Symbol', 'EPS Next 5Y in percent', 'DCF value', 'Current Price', 'Sector', 'Market Cap', 'Beta',
+              'Total Assets', 'Total Debt', 'Free Cash Flow', 'P/E Ratio', 'P/S Ratio', 'RSI', '10 day EMA',
+              '20 day SMA', '50 day SMA', '200 day SMA']
+    data = pd.DataFrame(results, columns=header)
+    data.to_csv(output_fname, index=False, header=header)
     # ....
 
     # end TODO
